63c06f5076438166ddcdbc21be40d7c51cf965e1
Rafael Santos <rf.sants001@gmail.com>
2023-05-03 21:31:34 -0300

    feat: Create Input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Ubuntu } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from "next/head";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "@/components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
     <>
       <Head>
         <title>Dash NX</title>
       </Head>
-      <h1>tested</h1>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          w="100%"
+          maxWidth={360}
+          bg="gray.600"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" type="email" label="Email" />
+            <Input name="password" type="password" label="Password" />
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme="twitter" size="lg">
+            Login
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 }
